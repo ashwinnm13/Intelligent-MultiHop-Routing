e4302813bf97c9b4fbcd8067c64f5d57d7ea5019
ashwinnm13 <ashwinnmuthu13@gmail.com>
2026-06-26 23:08:36 +0530

    train and inference

---
+++ rl_engine/inference.py
@@ -0,0 +1,40 @@
+from rl_engine.train import train
+
+## get the best route from trained agent
+def get_best_route(agent):
+
+    state = 0
+
+    route = [state]
+
+    while state != 5:
+
+        action = (
+            agent.q_table[state]
+            .argmax()
+        )
+
+        route.append(
+            action
+        )
+
+        state = action
+
+    return route
+
+
+if __name__ == "__main__":
+
+    agent = train()
+
+    route = (
+        get_best_route(
+            agent
+        )
+    )
+
+    print(
+        "\nBest Route:"
+    )
+
+    print(route)
---
+++ rl_engine/train.py
@@ -0,0 +1,62 @@
+from rl_engine.environment import NetworkEnv
+from rl_engine.q_learning import QLearningAgent
+
+
+EPISODES = 100
+
+
+def train():
+
+    env = NetworkEnv()
+
+    agent = QLearningAgent()
+
+    for episode in range(EPISODES):
+
+        state = env.reset()
+
+        done = False
+
+        while not done:
+
+            valid_actions = (
+                env.get_valid_actions(
+                    state
+                )
+            )
+
+            action = (
+                agent.choose_action(
+                    state,
+                    valid_actions
+                )
+            )
+
+            (
+                next_state,
+                reward,
+                done,
+                info,
+            ) = env.step(action)
+
+            agent.update(
+                state,
+                action,
+                reward,
+                next_state,
+            )
+
+            state = next_state
+
+    return agent
+
+
+if __name__ == "__main__":
+
+    trained_agent = train()
+
+    print("\nQ Table:\n")
+
+    print(
+        trained_agent.q_table
+    )
